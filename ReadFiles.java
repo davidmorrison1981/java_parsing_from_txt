42923f5e30bccc7375e78f368148e39a7672f0c5
davidmorrison1981 <davidmorrison@macbook-pro.home>
2017-02-15 21:03:49 +0000

    added array list

--- ReadFiles.java
+++ ReadFiles.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class ReadFiles 
 {
@@ -10,13 +10,18 @@ public class ReadFiles
         File inFile = new File("data.txt");
         BufferedReader reader = new BufferedReader(new java.io.FileReader(inFile));
         String line = null;
+        List<String> charArray = new ArrayList<String>();
 
         while ((line=reader.readLine()) != null)
                     {
                         String st  = line.replaceAll("\\s+","");
-                        char[] charArray = st.toCharArray();
-                        Arrays.sort(charArray);
+                        charArray=Arrays.asList(st);
+                        // Arrays.sort(charArray, Collections.reverseOrder());
                         System.out.println(charArray);
+
+                        // for(int i=0; i<charArray.length(); i++)
+                        //    String letter = "a"
+                        //    if letter ==
 
                     }
         }
