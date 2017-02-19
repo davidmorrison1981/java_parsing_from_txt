3b063b5c68b237f0ef189e49d96db69a03004b64
davidmorrison1981 <davidmorrison@macbook-pro.home>
2017-02-19 12:41:53 +0000

    modifying count and sort method

--- ReadFiles.java
+++ ReadFiles.java
@@ -9,14 +9,16 @@ public class ReadFiles
         {
         File inFile = new File("data.txt");
         BufferedReader reader = new BufferedReader(new java.io.FileReader(inFile));
-        String line = null;
-        List<String> charArray = new ArrayList<String>();
+        int[] counter = new int[26];
+        int totalLetters = 0;
+        int nonLetter = 0;
+        int next;
 
         while ((line=reader.readLine()) != null)
                     {
                         String st  = line.replaceAll("\\s+","");
                         charArray=Arrays.asList(st);
-                        // Arrays.sort(charArray, Collections.reverseOrder());
+                        Arrays.sort(charArray, Collections.reverseOrder());
                         System.out.println(charArray);
 
                         // for(int i=0; i<charArray.length(); i++)
