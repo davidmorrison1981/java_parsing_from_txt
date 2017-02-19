db572f69692507b120ad27f3d4dd238808f3edb0
davidmorrison1981 <davidmorrison@macbook-pro.home>
2017-02-19 12:53:56 +0000

    added map for sorting letters, no have to build function

--- ReadFiles.java
+++ ReadFiles.java
@@ -10,22 +10,24 @@ public class ReadFiles
         File inFile = new File("data.txt");
         BufferedReader reader = new BufferedReader(new java.io.FileReader(inFile));
         int[] counter = new int[26];
-        int totalLetters = 0;
+        int totalChars = 0;
         int nonLetter = 0;
-        int next;
+        int charNext;
 
-        while ((line=reader.readLine()) != null)
-                    {
-                        String st  = line.replaceAll("\\s+","");
-                        charArray=Arrays.asList(st);
-                        Arrays.sort(charArray, Collections.reverseOrder());
-                        System.out.println(charArray);
+       while ((charNext = reader.read()) != -1) {
+            char checkedChar = (char) charNext;
+            checkedChar = Character.toLowerCase(checkedChar);
+            if (checkedChar >= 'a' && checkedChar <= 'z') {
+                counter[checkedChar - 'a']++;
+                totalChars++;
+            } else {
+                nonLetter++;
+            }
+        }
 
-                        // for(int i=0; i<charArray.length(); i++)
-                        //    String letter = "a"
-                        //    if letter ==
+        Map letters = sorting(counter);  
+        printMap(letters);
 
-                    }
         }
         catch (IOException e)
         {
